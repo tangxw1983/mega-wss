d046b689f32347ccced366195ef1e45e3070df2b
tang <43070961@qq.com>
2017-03-06 22:31:04 +0800

    log/plugin路径修正

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.dbAdapter = 'mysql';
 //exports.dbAdapter = 'sqlite3';
-exports.logPath = './node_package/tmp';
-exports.keyPath = './node_package/tmp';
-exports.pluginPath = './node_package/plugins';
+exports.logPath = './tmp';
+exports.keyPath = './tmp';
+exports.pluginPath = './plugins';
